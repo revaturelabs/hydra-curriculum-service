b94db6640491d6a203e861ec619b166248428fd9
Bryton Hall <email@bryton.io>
2018-04-01 23:47:10 -0400

    uncomment documentation config

--- src/main/java/com/revature/hydra/curriculum/config/DocumentationConfig.java
+++ src/main/java/com/revature/hydra/curriculum/config/DocumentationConfig.java
@@ -11,12 +11,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class DocumentationConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.revature.hydra.curriculum.controller"))              
                 .paths(PathSelectors.any())
                 .build();
     }
